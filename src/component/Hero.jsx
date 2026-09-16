510801f95e79516554ae080bfd5eb5c20dadd0ff
kakaiimup <kailaaulia2209@gmail.com>
2026-09-16 15:58:51 +0700

    Update image source to use BASE_URL

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero({ title, subtitle }) {
   return (
     <section className="hero">
-      <img src="/image/profil.jpg.jpg" alt="Foto Profil" className="profile-photo"></img>
+      <img src={`${import.meta.env.BASE_URL}image/profil.jpg.jpg`} alt="Foto Profil" className="profile-photo"></img>
       <h1>{title}</h1>
       <p>{subtitle}</p>
     </section>
